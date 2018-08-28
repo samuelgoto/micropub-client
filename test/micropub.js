2d71e7d6066f8396a92bd4233d1a79f6ee83c90d
samuelgoto@gmail.com <samuelgoto@gmail.com>
2018-08-27 22:10:48 -0700

    adds some basic testing infrastructure

---
+++ test/micropub.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const Assert = require("assert");
+
+describe("micropub", function() {
+  it("hello world", async function() {
+   });
+
+  function assertThat(x) {
+   return {
+    equalsTo(y) {
+     Assert.equal(x.trim(), y.trim());
+    }
+   }
+  }
+ });
+
